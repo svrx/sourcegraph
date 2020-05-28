2c1b7ffee3eac46e9e189bf6555d8eb5a9665c43
Sergio Amaral <sergio.a.amaral@gmail.com>
2020-05-28 23:57:16 +0100

    Update version number for OSS build to 3.15.1

--- dev/dev-sourcegraph-server.sh
+++ dev/dev-sourcegraph-server.sh
@@ -7,6 +7,6 @@ set -ex
 # that this image is not exactly identical to the published sourcegraph/server
 # images, as those include Sourcegraph Enterprise features.
 time cmd/server/pre-build.sh
-IMAGE=sourcegraph/server:0.0.0-DEVELOPMENT VERSION=0.0.0-DEVELOPMENT time cmd/server/build.sh
+IMAGE=sourcegraph/server-oss:3.15.1 VERSION=3.15.1 time cmd/server/build.sh
 
-IMAGE=sourcegraph/server:0.0.0-DEVELOPMENT dev/run-server-image.sh
+IMAGE=sourcegraph/server-oss:3.15.1 dev/run-server-image.sh
