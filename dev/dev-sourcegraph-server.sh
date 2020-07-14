82109a4b9a1ff074713b481cb8d3510c87a65c87
Sergio Amaral <sergio.a.amaral@gmail.com>
2020-07-15 00:40:29 +0100

    Bump version to 3.15.2001

--- dev/dev-sourcegraph-server.sh
+++ dev/dev-sourcegraph-server.sh
@@ -7,6 +7,6 @@ set -ex
 # that this image is not exactly identical to the published sourcegraph/server
 # images, as those include Sourcegraph Enterprise features.
 time cmd/server/pre-build.sh
-IMAGE=svrx/sourcegraph-oss:3.15.1001 VERSION=3.15.1001 time cmd/server/build.sh
+IMAGE=svrx/sourcegraph-oss:3.15.2001 VERSION=3.15.2001 time cmd/server/build.sh
 
-IMAGE=svrx/sourcegraph-oss:3.15.1001 dev/run-server-image.sh
+IMAGE=svrx/sourcegraph-oss:3.15.2001 dev/run-server-image.sh
